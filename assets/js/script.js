21e2a8cc3e16c208201d5c67f1c600822c163014
Kyle Reynolds <kbreyn06@gmail.com>
2022-04-24 12:53:51 -0500

    added current date/time

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+//variables//
+var now = moment()
+
+//display current day's date at top of page//
+const currentDate = $('#currentDay');
+currentDate.text(now);
+
+//create elements that make up block calendar with current day at top//
+
+
+
+
+
+// append list item in time block//
+
+
+
+
+
+// save list item to local storage //
